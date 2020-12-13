7f2acd617a07418c996fdb4d2ae2ecca951bd38e
Jorge Trejo <jtrejo@mail.com>
2020-12-13 03:35:17 -0800

    added getRole to Intern, test passed

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -7,6 +7,10 @@ class Intern extends Employee{
         super(name, id, email);
         this.school = school
     }
+    
+    getRole() {
+        return "Intern"
+    }
 }
 
 module.exports = Intern
