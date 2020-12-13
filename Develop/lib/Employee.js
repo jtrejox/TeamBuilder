0818d663c6180c3e4ada15d0e86ee2128d10721d
Jorge Trejo <jtrejo@mail.com>
2020-12-13 03:19:17 -0800

    converted Employee to ES6

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,24 +1,30 @@
 // TODO: Write code to define and export the Employee class
-function Employee (name, id, email) {
-    this.name = name;
-    this.id = id;
-    this.email = email
+class Employee  {
+    constructor(name, id, email){
+        
+        this.name = name;
+        this.id = id;
+        this.email = email
+    }
+
+    getName() {
+        return this.name
+    };
+
+    getId()  {
+        return this.id
+    };
+    
+    getEmail() {
+        return this.email
+    };
+    
+    getRole()  {
+        return "Employee"
+    };
 }
 
-Employee.prototype.getName = function (){
-    return this.name
-};
 
-Employee.prototype.getId = function () {
-    return this.id
-};
 
-Employee.prototype.getEmail = function (){
-    return this.email
-};
-
-Employee.prototype.getRole = function () {
-    return "Employee"
-};
 
 module.exports = Employee
